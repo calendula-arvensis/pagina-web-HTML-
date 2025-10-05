485bcbdeb0a28f3855079644741f3dee834bcfc8
julietaalbaya-fai <julieta.albaya@est.fi.uncoma.edu.ar>
2025-10-05 09:09:07 -0300

    clase jueves 02/10, explicación de javascript

---
+++ ejemplo.js
@@ -0,0 +1,28 @@
+const miNumero = 2;
+const miBooleano = false;   //No puede cambiar
+const cadena = "hola mundo!";
+let miComidaPreferida = "Milanesa"; //Puede cambiar 
+miComidaPreferida = "Pancho"
+
+const misComidas = {}
+
+comidas; [ {
+    titulo: "pancho"
+    imagen: "https://..."
+},
+{
+    titulo: 
+}
+
+]
+
+//instalar "Node" para ejecutar
+if (miNumero > 3) {
+    //..
+    console.log("es mayor");
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
